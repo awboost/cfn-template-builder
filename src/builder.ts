8abdb0b0056ac24c6c32f74d7a716da3aeafb2aa
Gordon Leigh <gordon@propulsionworks.io>
2025-01-29 14:10:15 +0100

    remove TemplateSectionBuilder type

--- src/builder.ts
+++ src/builder.ts
@@ -2,10 +2,9 @@ import { Readable } from "node:stream";
 import type { Template, TemplateSection } from "./template.js";
 
 /**
- * Represents an object which can build a CloudFormation template using the
- * `add` function.
+ * Represents an object which can build a CloudFormation template.
  */
-export type TemplateSectionBuilder = {
+export type TemplateBuilder = {
   /**
    * Add the given element to the template. An exception will be thrown if an
    * element with the same name has already been added to the template.
@@ -15,12 +14,7 @@ export type TemplateSectionBuilder = {
     name: string,
     value: Required<Template>[Section][string],
   ) => void;
-};
 
-/**
- * Represents an object which can build a CloudFormation template.
- */
-export type TemplateBuilder = TemplateSectionBuilder & {
   /**
    * Use the given component.
    */
